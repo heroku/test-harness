288b0a7b860d0cec25fa96f08988a5b246314870
Michael & Josh <mike@hales.ws>
2014-10-14 20:08:56 +0000

    WIP: tick every second

--- main.go
+++ main.go
@@ -13,7 +13,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	sleepTime := r.Form["after"][0]
 	sleepDuration, _ := time.ParseDuration(sleepTime)
 	fmt.Printf("Sleeping for %q\n", sleepDuration)
+	ticker := time.NewTicker(1 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Fprintf(w, "tick\n")
+				if f, ok := w.(http.Flusher); ok {
+					f.Flush()
+				}
+			case <-stop:
+				return
+			}
+		}
+		// for t := range ticker.C {
+		// 	fmt.Fprintf(w, "tick\n")
+		// 	if f, ok := w.(http.Flusher); ok {
+		// 		f.Flush()
+		// 	}
+		// }
+	}()
 	time.Sleep(sleepDuration)
+	ticker.Stop()
 
 	fmt.Fprint(w, "OK")
 }
