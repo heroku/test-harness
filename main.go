1f4f5b4cbe0f35e2c8d4d51b12f06e1289c0a43e
Michael & Josh <mike@hales.ws>
2014-10-14 20:59:34 +0000

    simplify sleeping and make it second based only

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -11,24 +12,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	sleepTime := r.Form["after"][0]
-	sleepDuration, _ := time.ParseDuration(sleepTime)
-	fmt.Printf("Sleeping for %q\n", sleepDuration)
-	ticker := time.NewTicker(1 * time.Second)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				fmt.Fprintf(w, "tick\n")
-				if f, ok := w.(http.Flusher); ok {
-					f.Flush()
-				}
-			}
-		}
-	}()
-	time.Sleep(sleepDuration)
-	ticker.Stop()
 
-	fmt.Fprint(w, "OK")
+	fmt.Printf("Sleeping for %s\n", sleepTime)
+	sleepDuration, _ := strconv.Atoi(sleepTime)
+	for i := 0; i < sleepDuration; i++ {
+		time.Sleep(time.Second)
+		fmt.Println("tick")
+		fmt.Fprint(w, "tick\n")
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
+	}
+
+	fmt.Fprint(w, "OK\n")
 }
 
 func main() {
